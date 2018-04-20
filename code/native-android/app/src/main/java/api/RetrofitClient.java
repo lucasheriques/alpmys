d528f0b09b0143a14967e007f67a28264fe44592
felipeaugustosm <felipe.augustosm@gmail.com>
2018-04-20 11:15:56 -0300

    Initial version of the "Event List"

--- code/native-android/app/src/main/java/api/RetrofitClient.java
+++ code/native-android/app/src/main/java/api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String ROOT_URL = "http://192.168.25.4:45457/api/";
+    private static final String ROOT_URL = "http://192.168.56.1:45455/api/";
 
     private static Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
