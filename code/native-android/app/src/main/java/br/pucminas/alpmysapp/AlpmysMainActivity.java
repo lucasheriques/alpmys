d29cc5e79f1c7863ff4a4964fd8abf3e3d91dd58
felipeaugustosm <felipe.augustosm@gmail.com>
2018-04-24 13:31:18 -0300

    Função de Busca

--- code/native-android/app/src/main/java/br/pucminas/alpmysapp/AlpmysMainActivity.java
+++ code/native-android/app/src/main/java/br/pucminas/alpmysapp/AlpmysMainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SearchView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -35,6 +36,8 @@ import retrofit2.Response;
 public class AlpmysMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +53,7 @@ public class AlpmysMainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
