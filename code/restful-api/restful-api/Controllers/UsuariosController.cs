643ada1f5139289a2e5ee4faf3f0a6cb129a3745
lucasheriques <lucasheriques@gmail.com>
2018-05-03 01:47:01 -0300

    users controller

---
+++ code/restful-api/restful-api/Controllers/UsuariosController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestfulApi.Models;
+
+namespace RestfulApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Usuarios")]
+    public class UsuariosController : Controller
+    {
+        private readonly AlpmysContext _context;
+
+        public UsuariosController(AlpmysContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public IEnumerable<Usuario> GetUsuario()
+        {
+            return _context.Usuario;
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUsuario([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var usuario = await _context.Usuario.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // PUT: api/Usuarios/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuario([FromRoute] long id, [FromBody] Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        [HttpPost]
+        public async Task<IActionResult> PostUsuario([FromBody] Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Usuario.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuario);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var usuario = await _context.Usuario.SingleOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return Ok(usuario);
+        }
+
+        private bool UsuarioExists(long id)
+        {
+            return _context.Usuario.Any(e => e.Id == id);
+        }
+    }
+}
