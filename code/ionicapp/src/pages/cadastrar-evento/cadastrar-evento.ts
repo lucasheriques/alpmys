3ac112c1f112e15ed4fd9c039d70527e1698f759
ChristyanS <christyanrosa@hotmail.com>
2018-05-27 15:21:40 -0300

    Adding Create event

--- code/ionicapp/src/pages/cadastrar-evento/cadastrar-evento.ts
+++ code/ionicapp/src/pages/cadastrar-evento/cadastrar-evento.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
 
 /**
  * Generated class for the CadastrarEventoPage page.
@@ -13,12 +14,19 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cadastrar-evento.html',
 })
 export class CadastrarEventoPage {
-  evento = { nome: '', descricao: '', data: '' };
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  evento = { nome: '', descricao: '', data: '' ,duracao:'',linkImagem:'',linkPagina:'',local:{nome:'',descricao:'',cep:'',rua:'',numero:'',complemento:'',cidade:'',uf:''},usuarioId:'1'};
+  constructor(public navCtrl: NavController, public navParams: NavParams,public restProvider: RestProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CadastrarEventoPage');
   }
-
+  postEvento(){
+      this.restProvider.postEventos(this.evento).then((result) => {
+        console.log(result);
+      }, (err) => {
+        console.log(err);
+      });
+    
+  }
 }
--- code/ionicapp/src/providers/rest/rest.ts
+++ code/ionicapp/src/providers/rest/rest.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 /*
@@ -31,9 +31,11 @@ export class RestProvider {
       });
     });
   }
-  addEventos(data) {
+  postEventos(data) {
     return new Promise((resolve, reject) => {
-      this.http.post(this.apiUrl+'/Eventos', JSON.stringify(data))
+      this.http.post(this.apiUrl+'/Eventos', JSON.stringify(data),{
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+      })
         .subscribe(res => {
           resolve(res);
         }, (err) => {
